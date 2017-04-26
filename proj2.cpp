67b7bce66dea1a306a1a96c39b0bc171819545a3
Anfifo <afonsca-1995@hotmail.com>
2017-04-26 02:21:37 +0100

    pretty much all done but somehow getting SegFault and Abort on mooshak, fixed memory leaks in DisjointSets

--- proj2.cpp
+++ proj2.cpp
@@ -11,12 +11,11 @@ using namespace  std;
  * Andre Fonseca 84698	 *
  * Leonor Loureiro 84736 *
  *************************/
-#define AERO 1
-#define ROAD 2
+#define AERO 1 // used to reference the Airspace connection/edge
+#define ROAD 2 // used to reference a road connection/edge
 
 
 
-typedef  pair<int, int> nrConnections;
 
 /**
  * Class representation of an Edge
@@ -27,8 +26,9 @@ class Edge{
     	int predecessor;	// vertex from which edge originates from
     	int successor;		// vertex to which the edge is going to
     	int weight;         // weight of edge
-		int type;
+		int type;			// type: Airport connection or road
 	public:
+
 		/**
 		 * Default consctructor
 		 * @param pre the predecessor of the edge
@@ -41,20 +41,24 @@ class Edge{
 		int getType() const;
 };
 
-struct cheapestEdge
-{
-    inline bool operator() (const Edge& struct1, const Edge& struct2)
-    {
-		if(struct1.getWeight() == struct2.getWeight()){
-			return (struct1.getType()==ROAD);
-		}
-        return (struct1.getWeight() < struct2.getWeight());
-    }
-};
 
 /**
- * Structure used to model relations between 2 cities
- * This implementation of a directional graph is made through an adjacency list
+ * Comparison between 2 edges
+ * @return True if Edge1 is Smaller or Lesser than Edge 2
+ */
+bool sortEdges(const Edge& edge1, const Edge& edge2)
+{
+	if(edge1.getWeight() == edge2.getWeight()){
+		return (edge1.getType()==ROAD);
+	}
+    return (edge1.getWeight() < edge2.getWeight());
+}
+
+
+
+/**
+ * Structure used to model relations between severeral Cities
+ * This implementation of a undirected graph is made through it's Edges
  */
 class Graph{
 	private:
@@ -86,15 +90,29 @@ class Graph{
 		 * Based on Kruskal's algorithm for finding the minimum
 		 * spanning tree.
 		 */
-		pair<int,nrConnections> kruskalMST();
+		pair<int,pair<int,int> > kruskalModified();
 };
 
+/**
+ * Disjoint Sets CLass
+ * 2 sets are disjoints if there is no common element between them.
+ * This class models basic union, find and merge functions between disjoint sets.
+ */
 class DisjointSets{
 	private:
-		int *parent, *rank;
-		int nrElements;
+		int *parent; 			//the parent or representative of a set
+		int *rank;				//rank (number of merges) of a given set
+		int nrElements;			//number of elements in a given set
 	public:
-		DisjointSets(int n);
+
+		/**
+		 * Default constructor of DisjointSets class
+		 * It receives the number of Disjoint sets with which the object starts
+		 */
+		DisjointSets(int size);
+
+		~DisjointSets();
+
 		/**
 		 * Finds the parent of a node
 		 * @param u the node of which we want the parent
@@ -103,19 +121,28 @@ class DisjointSets{
 		void merge(int u, int v);
 };
 
-DisjointSets::DisjointSets(int n){
-	nrElements = n;
-	parent = new int[n];
-	rank = new int[n];
-	for(int i = 0; i < n; i++){
+
+
+
+
+DisjointSets::DisjointSets(int size){
+	nrElements = size;
+	parent = new int[size];
+	rank = new int[size];
+	for(int i = 0; i < size; i++){
 		parent[i] = i;
 		rank[i] = 0;
 	}
 }
 
+DisjointSets::~DisjointSets(){
+	delete [] parent;
+	delete [] rank;
+}
+
 int DisjointSets::find(int u){
 	if(u != parent[u]){
-		parent[u] = find(parent[u]);
+		parent[u] = find( parent[u] );
 	}
 	return parent[u];
 }
@@ -132,11 +159,12 @@ void DisjointSets::merge(int u, int v){
 	else{
 		parent[u] = v;
 	}
-
 	if(rank[u] == rank[v]){
 		rank[v]++;
 	}
 }
+
+
 
 Edge::Edge(int pre, int suc, int wei, int typ){
 	predecessor = pre;
@@ -145,32 +173,28 @@ Edge::Edge(int pre, int suc, int wei, int typ){
 	type = typ;
 }
 
-int Edge::getPredecessor() const{
-	return predecessor;
-}
+int Edge::getPredecessor() const{ return predecessor; }
 
-int Edge::getSucessor() const{
-	return successor;
-}
+int Edge::getSucessor() const{ return successor; }
 
-int Edge::getWeight() const{
-	return weight;
-}
+int Edge::getWeight() const{ return weight; }
 
-int Edge::getType() const{
-	return type;
-}
+int Edge::getType() const{ return type; }
+
+
+
+
+
 
 Graph::Graph(int v){
 	nrOfVertices = v;
 }
 
-
 void Graph::insertEdge(Edge e){
 	edges.push_back(e);
 }
 
-pair<int,nrConnections> Graph::kruskalMST(){
+pair<int,pair<int,int> > Graph::kruskalModified(){
 
 	//Weight of the past of miminum cost
 	int mstWeight = 0;
@@ -178,7 +202,7 @@ pair<int,nrConnections> Graph::kruskalMST(){
 	int nrAerosMST = 0;
 
 	//Orders the edges by weight
-	sort(edges.begin(), edges.end(), cheapestEdge());
+	sort(edges.begin(), edges.end(), sortEdges);
 
 	DisjointSets sets(nrOfVertices);
 
@@ -195,20 +219,17 @@ pair<int,nrConnections> Graph::kruskalMST(){
 
 			if((*i).getType() == ROAD){
 				nrRoadsMST++;
-			}
-			else{
+			}else{
 				nrAerosMST++;
 			}
 
 			mstWeight += (*i).getWeight();
-
 			sets.merge(u,v);
 		}
 	}
-
-
 	return make_pair(mstWeight,make_pair(nrAerosMST,nrRoadsMST));
 }
+
 
 
 
@@ -227,12 +248,16 @@ pair<int,nrConnections> Graph::kruskalMST(){
  * 	int: city1
  * 	int: city2
  * 	int: road's building cost
+ *
+ *
+ * Finds the number of airports and roads needed to be built between given cities
+ * in the cheapest possible way and the minimum airports required.
  */
 int inputProcess(){
 
-	int potentialAirports;
-	int potentialRoads;
-	int numberOfCities;
+	int potentialAirports = 0;
+	int potentialRoads = 0;
+	int numberOfCities = 0;
 	int airportVertex;
 	int buildingCost;
 	int airportID;
@@ -249,53 +274,54 @@ int inputProcess(){
 	Graph fullGraph (numberOfCities+1);
 
 //initialize all cities with no airports
-	for( i = 0; i < numberOfCities; i++)
+	for( i = 0; i < numberOfCities; i++){
 		airports[i] = -1;
+		roadsInCity[i] = 0;
+	}
 
 
 
-// airports
+// airports process
 	std::cin >> potentialAirports;
 	airportVertex = numberOfCities;
 
 	for(i = 0; i < potentialAirports; i++){
 		std::cin >> airportID >> buildingCost;
 
-		Edge airportEdge(airportID-1, airportVertex, buildingCost, AERO);
-		fullGraph.insertEdge(airportEdge);
-		airports[airportID] = buildingCost;
+		fullGraph.insertEdge(Edge (airportID-1, airportVertex, buildingCost, AERO));
+		airports[airportID-1] = buildingCost;
 	}
 
-// Roads
+// Roads process
 	std::cin >> potentialRoads;
 
 	for(i = 0; i < potentialRoads; i++){
 		std::cin >> city1 >> city2 >> buildingCost;
 
-		Edge roadEdge(city1-1, city2-1, buildingCost, ROAD);
-		fullGraph.insertEdge(roadEdge);
-		roadGraph.insertEdge(roadEdge);
+		fullGraph.insertEdge(Edge (city1-1, city2-1, buildingCost, ROAD));
+		roadGraph.insertEdge(Edge (city1-1, city2-1, buildingCost, ROAD));
 
-		roadsInCity[city1]++;
-		roadsInCity[city2]++;
+		roadsInCity[city1-1]++;
+		roadsInCity[city2-1]++;
 	}
 
 	for(i = 0; i < numberOfCities; i++){
 		if(roadsInCity[i] == 0){
 			if(airports[i] == -1){
 				std::cout << "Insuficiente" << '\n';
+				return 0;
 			}
 			//only do the complete Graph
 		}
 	}
 
 // Runs MST on each graph in order to choose which is cheapest
-	pair<int,nrConnections> roadsOutput = roadGraph.kruskalMST();
-	pair<int,nrConnections> fullOutput = fullGraph.kruskalMST();
+	pair<int,pair<int,int> > roadsOutput = roadGraph.kruskalModified();
+	pair<int,pair<int,int> > fullOutput = fullGraph.kruskalModified();
 
 // Verifies if the output was insuficient or not and prints it
-	if (roadsOutput.first > fullOutput.first || roadsOutput.second.second < numberOfCities -1){
-		if(fullOutput.second.first + fullOutput.second.second < numberOfCities){
+	if (roadsOutput.first > fullOutput.first || roadsOutput.second.second != numberOfCities -1){
+		if(fullOutput.second.first + fullOutput.second.second != numberOfCities){
 			std::cout << "Insuficiente" << '\n';
 		}else{
 			std::cout << fullOutput.first << '\n';
@@ -306,7 +332,6 @@ int inputProcess(){
 		std::cout << roadsOutput.second.first <<' '<< roadsOutput.second.second << '\n';
 	}
 
-
 	return 0;
 }
 
@@ -314,24 +339,6 @@ int inputProcess(){
 
 int main(int argc, char const *argv[])
 {
-	//int *aeroCost;          // cost of every aeroport
-	// Graph g(9);
-	// g.insertEdge(Edge(0,1,4,0));
-	// g.insertEdge(Edge(0,7,8,0));
-	// g.insertEdge(Edge(1,2,8,0));
-	// g.insertEdge(Edge(1,7,11,0));
-	// g.insertEdge(Edge(2,3,7,0));
-	// g.insertEdge(Edge(2,8,2,0));
-	// g.insertEdge(Edge(2,5,4,0));
-	// g.insertEdge(Edge(3,4,9,0));
-	// g.insertEdge(Edge(3,5,14,0));
-	// g.insertEdge(Edge(4,5,10,0));
-	// g.insertEdge(Edge(5,6,2,0));
-	// g.insertEdge(Edge(6,7,1,0));
-	// g.insertEdge(Edge(6,8,6,0));
-	// g.insertEdge(Edge(7,8,7,0));
-	// cout << g.kruskalMST().first <<endl;
-	//
 	inputProcess();
 	return 0;
 }
